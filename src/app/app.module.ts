35f57b2dd6ec59f0b20f27684f095814686a23ac
Abdoibrahim-98 <abdoibrahim19981998@gmail.com>
2024-04-30 15:40:36 +0300

    designing the header

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { SaleComponent } from './pages/sale/sale.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './pages/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    SaleComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
